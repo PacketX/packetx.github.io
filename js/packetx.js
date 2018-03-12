fa83bc1902722333c946d63890e75eaa601e75ca
proust-chuang <proust.chuang@hamicloud.net>
2018-03-12 22:45:10 +0800

    Update packetx.js

--- js/packetx.js
+++ js/packetx.js
@@ -3,17 +3,17 @@ var SLOGAN1 = [
 	'實體與虛擬化環境的流量監控',
 	'低風險流量(YouTube, NetFlix...)旁路機制',
 	'匯入威脅情資,執行惡意IP/Domain/URL阻斷',
-	'Netflow, DNS log, HTTP/HTTPS log, DPI log generation'
+	'網路流量L2-L7解析與Metadata產出'
 ];
 var SLOGAN2 = [
 	'Network Visibility for both physical and virtual environment(VMware & KVM)',
 	'In-line bypass low-threatening traffic such as multimedia streaming',
 	'Carrying out massive IP/domain/URL blocking according to external intelligence',
-	'Metadata generation: Netflow, DNS/HTTP/HTTPS log and App recognition log'
+	'Netflow, DNS/HTTP/HTTPS log and App recognition log'
 ];
 var INTERVAL = 5000;
 var ELAPSED = 800;
-var BG_COUNT = 3;
+var BG_COUNT = 4;
 var bgIndex = 0;
 $(document).ready(function(e) {
     $(".kv > div:visible").find('h1').html(SLOGAN1[0] + '<br/>' + SLOGAN2[0]);
